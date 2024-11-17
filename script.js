54d768ec451007a6278c564b048ac4b73860574d
skullcrusher0641 <skullcrusher0641@gmail.com>
2024-11-17 15:22:14 +0500

    login&signup-page

--- script.js
+++ script.js
@@ -1,70 +1,82 @@
-function setUsers () {
-let userName = document.getElementById("fullName").value;
-let userEmail = document.getElementById("email").value;
-let userPassword = document.getElementById("password").value;
+function setUsers() {
+    let userName = document.getElementById("fullName").value;
+    let userEmail = document.getElementById("email").value;
+    let userPassword = document.getElementById("password").value;
 
-let userData = {
-name: userName, email: userEmail, password: userPassword
+    let userData = {
+        name: userName, email: userEmail, password: userPassword
+    }
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    if (!userName || !userEmail || !userPassword) {
+        alert("enter field first")
+        return
+    }
+
+    if (users) {
+        for (i = 0; i < users.length; i++) {
+            if (userEmail == users[i].email) {
+                alert("email already exist")
+                document.getElementById("fullName").value = ""
+                document.getElementById("email").value = ""
+                document.getElementById("password").value = ""
+                return
+            }
+
+
+        }
+
+    }
+
+
+
+    if (users) {
+        users.push(userData)
+        localStorage.setItem("users", JSON.stringify(users))
+        document.getElementById("fullName").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("password").value = ""
+        window.location.href = "login.html"
+
+
+    }
+    else {
+        localStorage.setItem("users", JSON.stringify([userData]))
+        document.getElementById("fullName").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("password").value = ""
+        window.location.href = "login.html"
+
+    }
+
+console.log(userName)
+   
 }
-let users =  JSON.parse(localStorage.getItem("users"));
-
-if(!userName || !userEmail || !userPassword){
-alert("enter field first")
-return
-}
-
-if(users){
-for(i=0;i<users.length;i++){
-if(userEmail==users[i].email){
-    alert("email already exist")
-    return
-}
-
-
-}
-
-}
 
 
 
-if(users){
-users.push(userData)
-localStorage.setItem("users", JSON.stringify(users))
-window.location.href="login.html"
 
-}
-else{
-localStorage.setItem("users",JSON.stringify([userData]))
-window.location.href="login.html"
-}
-console.log(userData)
-}
-function getUsers(){
+function getUsers() {
+    let userEmail = document.getElementById("loginEmail").value;
+    let userPassword = document.getElementById("loginPassword").value;
+    let userData = JSON.parse(localStorage.getItem("users"))
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i].email == userEmail && userData[i].password == userPassword) {
+            alert("success")
 
+            window.location.href = "dashboard.html"
+        }
+        else if (userData == null) {
+            alert("register first")
 
+        }
+        else {
 
-}
-function getUsers () {
-let userEmail =document.getElementById("loginEmail").value;
-let userPassword =document.getElementById("loginPassword").value;
-let userData = JSON.parse(localStorage.getItem("users"))
-for (let i=0; i<userData.length; i++){
-if(userData[i].email== userEmail &&userData[i].password== userPassword ){
- alert("success")
+            alert("email invalid")
 
-window.location.href ="dashboard.html"
-}
-else if(userData==null){
-alert("register first")
-
-}
- else{
-
- alert ("email invalid")
-
- }
- break
-}
+        }
+        break
+    }
 }
 
 
